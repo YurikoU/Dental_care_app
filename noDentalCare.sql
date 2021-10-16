e9ef2f323328b3bf52a20d281e27e15382d0df91
YurikoU <76763897+YurikoU@users.noreply.github.com>
2021-10-15 21:25:01 -0700

    Add files via upload

--- noDentalCare.sql
+++ noDentalCare.sql
@@ -37,21 +37,16 @@ CREATE TABLE noDentalCares(
 -- );
 
 -- For chart 1 (output by age in the latest data)
-SELECT COUNT(researchYear)
-FROM noDentalCares
-WHERE researchYear = '2018';
-
-SELECT ageGroup, (COUNT(isDentalVisit) / 1258) * 100 AS "People who COUNLDN'T visited in 2018 (%)"
-FROM noDentalCares
-WHERE researchYear = '2018' AND isDentalVisit = false
-GROUP BY ageGroup 
-ORDER BY ageGroup ASC;
+-- SELECT ageGroup, COUNT(isDentalVisit)
+-- FROM noDentalCares
+-- WHERE researchYear = '2018' AND isDentalVisit = false
+-- GROUP BY ageGroup 
+-- ORDER BY IF(ageGroup RLIKE '^[a-zA-Z]', 1, 2), ageGroup;
 
 
 -- For chart 2 (output by researchYear)
-SELECT researchYear, COUNT(isDentalVisit) AS "Number of people who COUNLDN'T visit in each year"
-FROM noDentalCares
-WHERE isDentalVisit = false
-GROUP BY researchYear
-ORDER BY researchYear ASC;
-
+-- SELECT researchYear, COUNT(isDentalVisit)
+-- FROM noDentalCares
+-- WHERE isDentalVisit is false
+-- GROUP BY researchYear
+-- ORDER BY researchYear ASC;
